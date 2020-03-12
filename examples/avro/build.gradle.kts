0b81f5bf082e5a5a1a67230d4de50a3e7a13ecf0
sebastian.schmidt <bastman77@gmail.com>
2020-03-12 07:44:39 +0100

    initial

--- examples/avro/build.gradle.kts
+++ examples/avro/build.gradle.kts
@@ -12,12 +12,6 @@ plugins {
     application
 }
 
-repositories {
-    // Use jcenter for resolving dependencies.
-    // You can declare any Maven/Ivy/file repository here.
-    jcenter()
-}
-
 dependencies {
     // kotlin
     implementation(kotlin("stdlib-jdk8"))
@@ -48,13 +42,3 @@ configure<JavaPluginConvention> {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "1.8"
-            apiVersion = "1.3"
-            languageVersion = "1.3"
-            freeCompilerArgs = listOf("-Xjsr305=strict")
-        }
-    }
-}
